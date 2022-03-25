c3177711a0cd243536612336049c89139289f3de
Davide De Rosa <keeshux@gmail.com>
2022-03-25 09:34:46 +0100

    Upgrade to v5

--- scripts/net.rb
+++ scripts/net.rb
@@ -8,97 +8,93 @@ load "util.rb"
 
 ###
 
-servers = File.foreach("../template/servers.csv")
+template = File.foreach("../template/servers.csv")
 ca = File.read("../static/ca.crt")
 tls_wrap = read_tls_wrap("auth", 1, "../static/ta.key", 1)
 
 cfg = {
-    ca: ca,
-    wrap: tls_wrap,
-    ep: [
-        "UDP:443",
-        "UDP:80",
-        "UDP:53",
-        "UDP:1194",
-        "UDP:54783",
-        "TCP:443",
-        "TCP:587",
-        "TCP:21",
-        "TCP:22",
-        "TCP:80",
-        "TCP:143",
-        "TCP:3306",
-        "TCP:8080",
-        "TCP:54783",
-        "TCP:1194"
-    ],
-    cipher: "AES-256-GCM",
-    auth: "SHA512",
-    frame: 1,
-    compression: 0,
-    eku: true
-}
-
-external = {
-    hostname: "${id}.windscribe.com"
+  ca: ca,
+  tlsWrap: tls_wrap,
+  cipher: "AES-256-GCM",
+  digest: "SHA512",
+  compressionFraming: 1,
+  compressionAlgorithm: 0,
+  checksEKU: true
 }
 
 recommended = {
-    id: "default",
-    name: "Default",
-    comment: "256-bit encryption",
+  id: "default",
+  name: "Default",
+  comment: "256-bit encryption",
+  ovpn: {
     cfg: cfg,
-    external: external
+    endpoints: [
+      "UDP:443",
+      "UDP:80",
+      "UDP:53",
+      "UDP:1194",
+      "UDP:54783",
+      "TCP:443",
+      "TCP:587",
+      "TCP:21",
+      "TCP:22",
+      "TCP:80",
+      "TCP:143",
+      "TCP:3306",
+      "TCP:8080",
+      "TCP:54783",
+      "TCP:1194"
+    ]
+  }
 }
 presets = [recommended]
 
 defaults = {
-    :username => "abc1efg2-hijk345",
-    :pool => "us-central",
-    :preset => "default"
+  :username => "abc1efg2-hijk345",
+  :country => "US"
 }
 
 ###
 
-pools = []
-servers.with_index { |line, n|
-    id, country, area = line.strip.split(",")
-    id = id.downcase
-    hostname = "#{id}.windscribe.com"
+servers = []
+template.with_index { |line, n|
+  id, country, area = line.strip.split(",")
+  id = id.downcase
+  hostname = "#{id}.windscribe.com"
 
-    addresses = nil
-    if ARGV.include? "noresolv"
-        addresses = []
-        #addresses = ["1.2.3.4"]
+  addresses = nil
+  if ARGV.include? "noresolv"
+    addresses = []
+    #addresses = ["1.2.3.4"]
+  else
+    addresses = Resolv.getaddresses(hostname)
+  end
+  addresses.map! { |a|
+    IPAddr.new(a).to_i
+  }
+
+  server = {
+    :id => id,
+    :country => country
+  }
+  if !area.nil?
+    if area == "WINDFLIX"
+      server[:category] = area.downcase
     else
-        addresses = Resolv.getaddresses(hostname)
+      server[:area] = area
     end
-    addresses.map! { |a|
-        IPAddr.new(a).to_i
-    }
-
-    pool = {
-        :id => id,
-        :country => country
-    }
-    if !area.nil?
-        if area == "WINDFLIX"
-            pool[:category] = area.downcase
-        else
-            pool[:area] = area
-        end
-    end
-    pool[:hostname] = hostname
-    pool[:addrs] = addresses
-    pools << pool
+  end
+  server[:hostname] = hostname
+  server[:addrs] = addresses
+  servers << server
 }
 
 ###
 
 infra = {
-    :pools => pools,
-    :presets => presets,
-    :defaults => defaults
+  :servers => servers,
+  :presets => presets,
+  :defaults => defaults
 }
 
 puts infra.to_json
